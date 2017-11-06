86d45edf0096af9dcce01cb2771eeb6a868403b6
neveisa <smpdmohanty@gmail.com>
2017-11-06 22:51:42 +0530

    auto commit

--- NITR/tools/server_test.py
+++ NITR/tools/server_test.py
@@ -1,15 +1,49 @@
 import requests
 import sys
+import json
 
 
-servername="localhost:8080"
+servername="localhost:9090"
 
 if len(sys.argv)>1:
   servername=sys.argv[1]
 
-severname="http://"+servername
+servername="http://"+servername
 
 
-url=servername+"/api"
-payload={"action":"insert","payload":'{"device_id":"777"}'}
-r=requests.get(servername)
+endpoint=servername+"/api"
+
+
+print("Server endpoint --- %s " % endpoint)
+
+print("Enter to continue...")
+raw_input()
+
+testdevID="test_777"
+testlog = {"device_id":"%s"%testdevID,"hello":"world"}
+
+
+'''
+
+print("Inserting device %s" % testdevID)
+payload={"action":"insertdevice","payload":'{"device_id":"%s"}' % testdevID}
+r=requests.get(endpoint,params=payload)
+print r.text
+
+raw_input()
+
+'''
+
+for count in range(100):
+	testlog["logID"]=count
+	print("Inserting log... %s" % testlog)
+
+	payload={"action":"insertlog","payload":json.dumps(testlog)}
+
+
+	r=requests.get(endpoint,params=payload)
+	print (r.text)
+
+	print("")
+
+print ("\ntest complete....")
